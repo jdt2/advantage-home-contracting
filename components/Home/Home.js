6d661d77d5274535efc54cc5099e3ef654d3b763
Jesse Du <jessedu@Jesses-MacBook-Pro.local>
2019-05-21 17:41:15 -0400

    Adjusted content based on suggestions

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -34,7 +34,7 @@ export default class Home extends React.Component {
             <View>
                 <Card style={styles.cardSpace}>
                     <CardItem header bordered style={styles.container}>
-                        <Text style={{fontSize: 36, color: '#5987a7'}}>Our Mission</Text>
+                        <Text style={{fontSize: 36, color: '#5987a7', textAlign:'center'}}>Mission Statement</Text>
                     </CardItem>
                     <CardItem>
                         <Body style={styles.container}>
@@ -44,17 +44,23 @@ export default class Home extends React.Component {
                 </Card>
                 <Card style={styles.cardSpace}>
                     <CardItem header bordered style={styles.container}>
-                        <Text style={{fontSize: 36, color: '#5987a7'}}>What We Can Do</Text>
+                        <Text style={{fontSize: 36, color: '#5987a7', textAlign: 'center'}}>The Advantage Difference</Text>
                     </CardItem>
                     <CardItem>
                         <Body style={styles.container}>
-                            <Text style={styles.center}>Advantage Home Contracting is your premier home contractor. We are there for you whether the project is big or small. Our team of in-house design and building professionals are fully licensed with no sub-contractors or outsourcing. When you work with Advantage you work with Advantage! All our professionals have at least 10 years of experience and we provide a 100% guarantee on our work with no mark-up on material cost. </Text>
+                            <Text style={styles.center}>
+                                {/*Advantage Home Contracting is your premier home contractor. We are there for you whether the project is big or small. Our team of in-house design and building professionals are fully licensed with no sub-contractors or outsourcing. When you work with Advantage you work with Advantage! All our professionals have at least 10 years of experience and we provide a 100% guarantee on our work with no mark-up on material cost.*/}
+                                -Employees (not subcontractors) do 99% of all our work. Our employees have a minimum of 15 years experience.{"\n\n"}
+                                -8 Divisions to serve nearly every Home Improvement/Repair or Remodeling project.{"\n\n"}
+                                -In house design team to provide basic architectural drawings to interior design and coordination of any project.{"\n\n"}
+                                The areas most awarded contractor with a BBB A+ Rating
+                            </Text>
                         </Body>
                     </CardItem>
                 </Card>
                 <Card style={styles.cardSpace}>
                     <CardItem header bordered style={styles.container}>
-                        <Text style={{fontSize: 36, color: '#5987a7'}}>Free Estimate</Text>
+                        <Text style={{fontSize: 36, color: '#5987a7', textAlign: 'center'}}>Free Estimate</Text>
                     </CardItem>
                     <CardItem>
                         <Body style={styles.container}>
@@ -71,34 +77,37 @@ export default class Home extends React.Component {
             <View>
                 <Card style={styles.cardSpace}>
                     <CardItem header bordered style={styles.container}>
-                        <Image style={styles.serviceIcon} source={require('../../assets/serviceIcons/level-1.png')} />
-                        <Text style={{fontSize: 36, color: '#5987a7'}}>Tile Masonry</Text>
-                    </CardItem>
-                    <CardItem>
-                        <Body style={styles.container}>
-                            <Text>The areas most experienced and talented tile and masonry company able to repair and install brick and or tile making your desired look perfect</Text>
-                        </Body>
-                    </CardItem>
-                </Card>
-                <Card style={styles.cardSpace}>
-                    <CardItem header bordered style={styles.container}>
-                        <Image style={styles.serviceIcon} source={require('../../assets/serviceIcons/level-2.png')} />
-                        <Text style={{fontSize: 36, color: '#5987a7'}}>Roofing and Siding</Text>
-                    </CardItem>
-                    <CardItem>
-                        <Body style={styles.container}>
-                            <Text>We offer superior roofing systems with enhanced warranties. Advantage Home Contracting is your GAF Certified Weather Stopper Roofing Contractor.</Text>
-                        </Body>
-                    </CardItem>
-                </Card>
-                <Card style={styles.cardSpace}>
-                    <CardItem header bordered style={styles.container}>
-                        <Image style={styles.serviceIcon} source={require('../../assets/serviceIcons/level-3.png')} />
-                        <Text style={{fontSize: 36, color: '#5987a7'}}>Kitchen and Bathrooms</Text>
+                        <Image style={styles.serviceIcon} source={require('../../assets/serviceIcons/level-7.png')} />
+                        <Text style={{fontSize: 36, color: '#5987a7'}}>Handyman</Text>
                     </CardItem>
                     <CardItem>
                         <Body style={styles.serviceIcon} style={styles.container}>
-                            <Text>The areas most complete Kitchen and Bathroom Remodeling team from design and installation to final completion.</Text>
+                            <Text>No job to big or too small from simple repairs, installations to everything you can imagine</Text>
+                        </Body>
+                    </CardItem>
+                </Card>
+                <Card>
+                    <CardItem header bordered style={styles.container}>
+                        <Image style={styles.serviceIcon} source={require('../../assets/serviceIcons/level-8.png')} />
+                        <Text style={{fontSize: 36, color: '#5987a7'}}>Painting</Text>
+                    </CardItem>
+                    <CardItem>
+                        <Body style={styles.serviceIcon} style={styles.container}>
+                            <Text>We off custom interior to cabinet repaint/re-stain to exteriors as well as metal roofs.
+                                {"\n"}We also have an on staff designer to assist with color selections if needed
+                            </Text>
+                        </Body>
+                    </CardItem>
+                </Card>
+                <Card style={styles.cardSpace}>
+                    <CardItem header bordered style={styles.container}>
+                        <Image style={styles.serviceIcon} source={require('../../assets/serviceIcons/level-6.png')} />
+                        <Text style={{fontSize: 36, color: '#5987a7'}}>Interior Remodeling{/*  (Baths, Kitchen, Basements and more) */}</Text>
+                    </CardItem>
+                    <CardItem>
+                        <Body style={styles.serviceIcon} style={styles.container}>
+                            <Text>We can help you update your bathrooms, kitchen and finish that basement for much needed space. We can re-locate a laundry room or just expand and open up rooms for that more modern floor plan. We have an on staff designer to make your project feel effortless and stress free.
+                            </Text>
                         </Body>
                     </CardItem>
                 </Card>
@@ -109,7 +118,51 @@ export default class Home extends React.Component {
                     </CardItem>
                     <CardItem>
                         <Body style={styles.serviceIcon} style={styles.container}>
-                            <Text>Make your outdoor living space a reality. Enjoy the outdoors regardless of the season or the weather with a screened in porch or a deck to experience the best views from your house.</Text>
+                            <Text>We can build or expand your existing deck and porch as well as rebuild it with Trex and or composite material of your choice. We can re-stain, rebuild or repair any deck or porch. We also offer our Turtle Deck refinishing system with its industry leading 5 year warranty.</Text>
+                        </Body>
+                    </CardItem>
+                </Card>
+                <Card style={styles.cardSpace}>
+                    <CardItem header bordered style={styles.container}>
+                        <Image style={styles.serviceIcon} source={require('../../assets/serviceIcons/level-2.png')} />
+                        <Text style={{fontSize: 36, color: '#5987a7'}}>Roofing and Siding</Text>
+                    </CardItem>
+                    <CardItem>
+                        <Body style={styles.container}>
+                            <Text>GAF certifies as well a James Hardy approved installer. We are the areas premier residential roofing and siding contractor. We can provide traditional shingle installs to metal roofing.</Text>
+                        </Body>
+                    </CardItem>
+                </Card>
+                <Card style={styles.cardSpace}>
+                    <CardItem header bordered style={styles.container}>
+                        <Image style={styles.serviceIcon} source={require('../../assets/serviceIcons/level-1.png')} />
+                        <Text style={{fontSize: 36, color: '#5987a7'}}>Tile</Text>
+                    </CardItem>
+                    <CardItem>
+                        <Body style={styles.container}>
+                            <Text>We have the most talented tile installers on our employee team so whether you want a new backsplash for your kitchen an updated tile floor or shower, we are your local proven choice.</Text>
+                        </Body>
+                    </CardItem>
+                </Card>
+                <Card style={styles.cardSpace}>
+                    <CardItem header bordered style={styles.container}>
+                        <Image style={styles.serviceIcon} source={require('../../assets/serviceIcons/level-1.png')} />
+                        <Text style={{fontSize: 36, color: '#5987a7'}}>Masonry</Text>
+                    </CardItem>
+                    <CardItem>
+                        <Body style={styles.container}>
+                            <Text>We can provide all your basic masonry repairs to chimney and steps.</Text>
+                        </Body>
+                    </CardItem>
+                </Card>
+                {/*<Card style={styles.cardSpace}>
+                    <CardItem header bordered style={styles.container}>
+                        <Image style={styles.serviceIcon} source={require('../../assets/serviceIcons/level-3.png')} />
+                        <Text style={{fontSize: 36, color: '#5987a7'}}>Kitchen and Bathrooms</Text>
+                    </CardItem>
+                    <CardItem>
+                        <Body style={styles.serviceIcon} style={styles.container}>
+                            <Text>The areas most complete Kitchen and Bathroom Remodeling team from design and installation to final completion.</Text>
                         </Body>
                     </CardItem>
                 </Card>
@@ -123,40 +176,7 @@ export default class Home extends React.Component {
                             <Text>Advantage Home Contracting holds a VA Class A Contractors License with a Builders designation. Offering the areas most comprehensive design and build staff providing permit and drawings assistance to complete project oversight and execution.</Text>
                         </Body>
                     </CardItem>
-                </Card>
-                <Card style={styles.cardSpace}>
-                    <CardItem header bordered style={styles.container}>
-                        <Image style={styles.serviceIcon} source={require('../../assets/serviceIcons/level-6.png')} />
-                        <Text style={{fontSize: 36, color: '#5987a7'}}>Remodeling (Interior)</Text>
-                    </CardItem>
-                    <CardItem>
-                        <Body style={styles.serviceIcon} style={styles.container}>
-                            <Text>Do you want to change the overall look of your home and make it more functional as well as breathtaking?</Text>
-                        </Body>
-                    </CardItem>
-                </Card>
-                <Card style={styles.cardSpace}>
-                    <CardItem header bordered style={styles.container}>
-                        <Image style={styles.serviceIcon} source={require('../../assets/serviceIcons/level-7.png')} />
-                        <Text style={{fontSize: 36, color: '#5987a7'}}>Handyman</Text>
-                    </CardItem>
-                    <CardItem>
-                        <Body style={styles.serviceIcon} style={styles.container}>
-                            <Text>Advantage Home Contracting can do any job from large to very small. Whatever your needs, we are the contracting service for you.</Text>
-                        </Body>
-                    </CardItem>
-                </Card>
-                <Card>
-                    <CardItem header bordered style={styles.container}>
-                        <Image style={styles.serviceIcon} source={require('../../assets/serviceIcons/level-8.png')} />
-                        <Text style={{fontSize: 36, color: '#5987a7'}}>Painting</Text>
-                    </CardItem>
-                    <CardItem>
-                        <Body style={styles.serviceIcon} style={styles.container}>
-                            <Text>Why completely renovate, when a fresh coat of paint will do?</Text>
-                        </Body>
-                    </CardItem>
-                </Card>
+                </Card>*/}
             </View>
         );
     }
@@ -166,11 +186,12 @@ export default class Home extends React.Component {
             <View>
                 <Card style={styles.cardSpace}>
                     <CardItem header bordered style={styles.container}>
-                        <Text style={{fontSize: 36, color: '#5987a7'}}>Where Are We?</Text>
+                        {/* <Text style={{fontSize: 36, color: '#5987a7'}}>Where Are We?</Text> */}
+                        <Image style={{width: 320, height: 300}} source={require('../../assets/icons/AHCStaffPhoto.jpg')} />
                     </CardItem>
                     <CardItem>
                         <Body style={styles.container}>
-                            <MapView
+                            {/* <MapView
                                 initialRegion={{
                                     latitude: 38.049172,
                                     longitude: -78.467938,
@@ -189,25 +210,44 @@ export default class Home extends React.Component {
                                     title="Advantage Home Contracting"
                                     description="516 Brookway Drive"
                                 />
-                            </MapView>
-                            <Text style={styles.center}><Text style={{fontWeight: 'bold'}}>Our address</Text>
+                            </MapView> */}
+                            <Text style={styles.center}>
+                                Servicing Charlottesville and surrounding area since 2000{"\n"}
+                            </Text>
+                            <View style={{flex:1,height:100,justifyContent: 'center', alignItems: 'center',flexDirection: 'row'}}>
+                                <Image style={{flex:1, resizeMode: 'contain', marginRight: 10}} source={require('../../assets/icons/ReadersChoice.jpg')} />
+                                <Image style={{flex:1, resizeMode: 'contain', marginRight: 10}} source={require('../../assets/icons/BestinCVille.jpg')} />
+                                <Image style={{flex:1, resizeMode: 'contain'}} source={require('../../assets/icons/CharlottesvilleFamilyAward.jpg')} />
+                            </View>
+                            <Text style={styles.center}>
+                                {"\n"}BBB A+ Rating{"\n"}
+                            </Text>                                
+
+                            <Image style={{width: 127, height: 140}} source={require('../../assets/icons/BBBRating.jpg')} />
+
+                            <Text style={styles.center}>
+                            {"\n"}Locally owned and operated{"\n"}
+                            </Text>
+
+                            <Image style={{width: 320, height: 240}} source={require('../../assets/icons/AHCOffice.jpg')} />
+                            <Text style={[styles.center, {marginTop: 20,}]}>
+                            <Text style={{fontWeight: 'bold', textAlign: 'center'}}>Our address</Text>
 {'\n'}516 Brookway Drive
 {'\n'}Charlottesville, VA 22901
 {'\n'}P: 434-817-7222
-{'\n'}F: 434-817-7220
-{'\n\n'}Contact us at:
-{'\n'}<Text style={{color: 'blue'}} onPress={() => Linking.openURL('mailto:advantagehandy@yahoo.com')}>advantagehandy@yahoo.com</Text>
-</Text>
+{'\n\n'}Website:
+{'\n'}<Text style={{color: 'blue'}} onPress={() => Linking.openURL('www.advantagehandy.com')}>www.advantagehandy.com
+</Text></Text>
                         </Body>
                     </CardItem>
                 </Card>
                 <Card>
                     <CardItem header bordered style={styles.container}>
-                        <Text style={{fontSize: 36, color: '#5987a7'}}>Credits</Text>
+                        <Text style={{fontSize: 36, color: '#5987a7', textAlign: 'center'}}>Credits</Text>
                     </CardItem>
                     <CardItem>
                         <Body style={styles.container}>
-                            <Text>Developed By: {'\n'}Jesse Du{'\n'}Sean Castrina{'\n'}Collin Castrina{'\n\n'}Published By: {'\n'}Champion Publishing</Text>
+                            <Text style={{textAlign: 'center'}}>Developed By: {'\n'}Jesse Du{'\n'}Sean Castrina{'\n'}Collin Castrina{'\n\n'}Published By: {'\n'}Champion Publishing</Text>
                         </Body>
                     </CardItem>
                 </Card>
@@ -221,15 +261,19 @@ export default class Home extends React.Component {
         return (
             <Container>
                     <View style={{backgroundColor: "white", height: 20}} />
-                    <Image
-                        source={require('../../assets/logoCrop.png')}
-                        style={{height: 170,width: '100%'}}
-                    />
+                    <View
+                        style={{backgroundColor:'#3A5872',alignItems:'center'}}
+                    >
+                        <Image
+                            source={require('../../assets/logo.png')}
+                            style={{height: 100,width: '50%'}}
+                        />
+                    </View>
                     <Tabs
                         locked
                         renderTabBar={() => <ScrollableTab />}
                     >
-                        <Tab heading="What We Do" style={styles.tab}>
+                        <Tab heading="The Advantage Difference" style={styles.tab}>
                             <ScrollView>
                                 {this.tab1()}
                             </ScrollView>
